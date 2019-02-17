53f4cd5578d1141f380c4a8505fae122b8d0bad9
Max <dranik15@mail.ru>
2019-02-17 21:40:19 +0300

    Update IntegerExtensions.cs

--- IntegerExtensions.cs
+++ IntegerExtensions.cs
@@ -6,6 +6,12 @@ namespace PadawansTask3
     {
         public static int Gcd(int a, int b)
         {
+            if (a==0 && b==0)
+            {
+                throw new ArgumentException();
+            }
+            a = Math.Abs(a);
+            b = Math.Abs(b);
             while(a!=0 && b!=0)
             {
                 if (a>b)
